f275262f3e6ac978fe173456d8213a7466beeeb6
Tri Ardini <tr.ardn@gmail.com>
2020-06-24 11:30:11 +0700

    Initial Commit

---
+++ DotaItemApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DotaItemApp
+//
+//  Created by Tri Ardini on 24/06/20.
+//  Copyright © 2020 BuruhKoding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
