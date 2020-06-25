459b271084e8e0585d80e87a3166acbe1945e5ea
Tri Ardini <tr.ardn@gmail.com>
2020-06-25 20:41:34 +0700

    revision from latest submission

--- DotaItemApp/Controller/DetailViewController.swift
+++ DotaItemApp/Controller/DetailViewController.swift
@@ -13,29 +13,15 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var itemImage: UIImageView!
     @IBOutlet weak var itemDesc: UILabel!
     
-    // Digunakan untuk menampung data Item
     var item: Item?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Digunakan untuk menetapkan nilai item ke beberapa view yang ada
         if let result = item {
             itemName.text = result.name
             itemDesc.text = result.description
             itemImage.image = result.image
         }
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- DotaItemApp/Controller/ViewController.swift
+++ DotaItemApp/Controller/ViewController.swift
@@ -15,20 +15,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Menambahkan title pada navigation
         self.navigationItem.title = "Dota 2 Items"
         
-        // let buttonProfile = UIBarButtonItem(image: UIImage(named: "profile"), style: .done, target: self, action: nil)
         let buttonProfile = UIBarButtonItem(title: "Profile", style: .done, target: self, action: #selector(ViewController.tapProfileButton))
         self.navigationItem.rightBarButtonItem = buttonProfile
         
-        // Menghubungkan itemTableView dengan kedua metode dibawah
         heroTableView.dataSource = self
         
-        // Menambahkan delegate ke table view
         heroTableView.delegate = self
         
-        // Menghubungkan berkas XIB untuk DotaItemTableViewCell dengan itemTableView
         heroTableView.register(UINib(nibName: "DotaItemTableViewCell", bundle: nil), forCellReuseIdentifier: "ItemCell")
     }
     
@@ -42,25 +37,19 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // Digunakan untuk menambahkan total item yang akan muncul di dalam Table View
         return items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Digunakan untuh menghubungkan cell dengan identifier "ItemCell"
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! DotaItemTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as? DotaItemTableViewCell
         
-        // Menetapkan nilai item ke view di dalam cell
         let item = items[indexPath.row]
-        cell.itemName.text = item.name
-        cell.itemDesc.text = item.description
-        cell.itemImage.image = item.image
+        cell?.itemName.text = item.name
+        cell?.itemDesc.text = item.description
+        cell?.itemImage.image = item.image
+        cell?.itemImage.clipsToBounds = true
         
-        // Kode ini digunakan untuk membuat imageView memiliki frame bound/lingkaran
-        cell.itemImage.layer.cornerRadius = cell.itemImage.frame.height / 2
-        cell.itemImage.clipsToBounds = true
-        
-        return cell
+        return cell ?? DotaItemTableViewCell()
     }
     
     
@@ -68,13 +57,12 @@ extension ViewController: UITableViewDataSource{
 
 extension ViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // Memanggil view controller dengan berkas NIB/XIB didalamnya
         let detail = DetailViewController(nibName: "DetailViewController", bundle: nil)
         
-        // Mengirim data item
         detail.item = items[indexPath.row]
         
-        // Push/mendorong view controller lain
+        tableView.deselectRow(at: indexPath, animated: false)
+        
         self.navigationController?.pushViewController(detail, animated: true)
     }
     
